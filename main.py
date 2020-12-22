003d491906c0165167409257e728b895f6939239
noraahlman23 <819446@mystma.org>
2020-12-22 16:38:20 +0000

    test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-#WRITE YOUR CODE IN THIS FILE
+def name():
+    return ("Nora")
+
+print (name)
+
+def favorites(favCol,favNum):
+    return (favCol, favNum)
+
+print (favorites("pink","16"))
+
+def bigadd(x,y,z,a,b):
+    return (x + y + z + a + b)
+
+print (bigadd(1,2,3,4,5))
+
+def fourOperations(x,y,z,a,b):
+    return (x + y - z * a - b)
+
+print (fourOperations(2,3,4,6,5))
